aa4f27b5d2ff74d7679d6aa7430de9ddaf2d5c41
Lucas Berbel <lj.beneb@gmail.com>
2023-09-14 23:42:41 +0000

    salvando os arquivos '

--- app/src/main/java/application/controller/IndexController.java
+++ app/src/main/java/application/controller/IndexController.java
@@ -7,7 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
     @RequestMapping("/")
     public String home() {
-        return "WEB-INF/home.jsp";
+        //return "WEB-INF/home.jsp";
+        return home;
     }
 }
 
