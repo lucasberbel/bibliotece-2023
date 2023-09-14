f32566249cab0b533117a0f8434a025ce337d208
Lucas Berbel <lj.beneb@gmail.com>
2023-09-14 23:27:35 +0000

    salvando os arquivos '

---
+++ app/src/main/java/application/model/Genero.java
@@ -0,0 +1,28 @@
+package application.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name='generos')
+public class Genero {
+    @Id
+    @GeneratedValue (strategy=GenerationType.IDENTITY)
+    private int id;
+    private String nome;
+
+    public int GetId(){
+        return Id;
+    }
+
+    public void setId(int Id){
+        this.id = id;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+}
