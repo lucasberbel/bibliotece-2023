c2405395bf197f48bec2798c6397bcab6e15a899
Lucas Berbel <lj.beneb@gmail.com>
2023-09-15 01:00:19 +0000

    salvando os arquivos

--- app/src/main/java/application/controller/GeneroController.java
+++ app/src/main/java/application/controller/GeneroController.java
@@ -5,3 +5,41 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import application.model.Genero;
+import application.model.GeneroRepository;
+
+@controller
+@RequestMapping("/genero")
+public class GeneroController{
+    @Autowired
+    private GeneroRepository generoRepo;
+
+    @RequestMapping ("/list")
+    public String list (Model model)
+    model.addAtribute("generos", generoRepo.findAll());
+    return "/genero/list";
+
+    @RequestMapping("/insert")
+    public String insert(){
+        return "/genero/insert";
+
+    @RequestMapping(value = "/insert", method=RequestMethod.POST)
+    public String insert (@RequestParam("nome") String nome){
+        Genero genero = new Genero();
+        genero.setNome(nome);
+
+        generoRepo.save(genero);
+
+        return "redirect:/genero/list";
+        
+         
+
+
+
+    }
+    }
+
+
+}
